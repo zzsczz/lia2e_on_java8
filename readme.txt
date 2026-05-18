c8cf7c0ad7d6038c0940e14feb632791e5cff35a
zzsczz <13802323@qq.com>
2026-05-18 20:01:34 +0800

    todo  add lib jar to class path

---
+++ readme.txt
@@ -0,0 +1,7 @@
+﻿simple proj with ant
+
+java utf-8
+
+readme utf-8 + bom
+
+ant build.xml
---
+++ src/j1.java
@@ -0,0 +1,10 @@
+public class j1
+{
+	public static void main (String args[]){
+	
+		System.out.println("hello world");
+	
+	
+	}
+
+}
